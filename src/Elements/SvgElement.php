428bc8960897f40c62ca21101d1972d1d2305ec1
Yann Di Serio <diserio.yann@gmail.com>
2016-12-13 11:48:33 +0000

    Delete useless properties.

--- src/Elements/SvgElement.php
+++ src/Elements/SvgElement.php
@@ -24,24 +24,6 @@ class SvgElement extends AbstractElement
 	 * @var WidthAttr $width : All SVG's width.
 	 */
 	protected $width;
-	/**
-	 * https://www.w3.org/TR/SVG/struct.html#RequiredFeaturesAttribute
-	 * 
-	 * @var RequiredFeaturesAttr $requiredFeatures : The value is a list of feature strings.
-	 */
-	protected $requiredFeatures;
-	/**
-	 * https://www.w3.org/TR/SVG/struct.html#RequiredExtensionsAttribute
-	 * 
-	 * @var RequiredExtensionAttr $requiredExtensions : The value is a list of IRI references which identify the required extensions.
-	 */
-	protected $requiredExtensions;
-	/**
-	 * https://www.w3.org/TR/SVG/struct.html#SystemLanguageAttribute
-	 * 
-	 * @var SystemLanguageAttr $systemLanguage : Evaluates to "true" if one of the languages indicated by user preferences exactly equals one of the languages given in the value of this parameter.
-	 */
-	protected $systemLanguage;
 	
 	public function __construct() {
 		parent::__construct();
